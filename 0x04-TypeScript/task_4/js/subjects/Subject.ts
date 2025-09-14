edb3e4b60c3a734949e28d58530a519caff72f4e
chidera Ikechukwu <bestbrain407@gmail.com>
2025-09-14 11:44:59 +0000

    made some changes

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,16 +1,10 @@
 /// <reference path="./Teacher.ts" />
 
 namespace Subjects {
-  // The interface is optional if the test doesn't require it, but safe to include
-  export interface SubjectInterface {
-    teacher: Teacher;
-    setTeacher(teacher: Teacher): void;
-  }
-
-  export class Subject implements SubjectInterface {
+  export class Subject {
     teacher!: Teacher;
 
-    setTeacher(teacher: Teacher): void {
+    setTeacher(teacher:Teacher) {
       this.teacher = teacher;
     }
   }
