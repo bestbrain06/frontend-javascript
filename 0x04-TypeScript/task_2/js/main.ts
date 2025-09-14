bd0156d814c2e375921cc81eeae0659ac27f6952
chidera Ikechukwu <bestbrain407@gmail.com>
2025-09-14 10:59:14 +0000

    made some changes

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -2,42 +2,49 @@ export type Director = {
     name: string;
     salary: number;
     role: "director";
+    workDirectorTasks: () => string;
 };
 
 export type Teacher = {
     name: string;
     salary: number;
     role: "teacher";
+    workTeacherTasks: () => string;
 };
 
 export type Employee = Director | Teacher;
 
 export function createEmployee(salary: number): Employee {
     if (salary > 500) {
-        return { name: "Emmanuel", salary, role: "director" };
+        return {
+            name: "Emmanuel",
+            salary,
+            role: "director",
+            workDirectorTasks: () => "Getting to director tasks",
+        };
     } else {
-        return { name: "Ama", salary, role: "teacher" };
+        return {
+            name: "Ama",
+            salary,
+            role: "teacher",
+            workTeacherTasks: () => "Getting to work",
+        };
     }
 }
 
-// Exported type predicate
 export function isDirector(employee: Employee): employee is Director {
     return employee.role === "director";
 }
 
-export function workDirectorTasks(): void {
-    console.log("Getting to director tasks");
-}
 
-export function workTeacherTasks(): void {
-    console.log("Getting to work");
-}
-
-// Exported function
-export function executeWork(employee: Employee): void {
+export function executeWork(employee: Employee): string {
     if (isDirector(employee)) {
-        workDirectorTasks();
+        return employee.workDirectorTasks();
     } else {
-        workTeacherTasks();
+        return employee.workTeacherTasks();
     }
 }
+
+
+console.log(executeWork(createEmployee(200)));  
+console.log(executeWork(createEmployee(1000)));  
